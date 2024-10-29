88fec0403c838adabcf3f6ad180b95481c7fd120
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-29 19:31:36 +0100

    Feat: Add custom colors to tailwind css config.

--- store/tailwind.config.js
+++ store/tailwind.config.js
@@ -2,6 +2,40 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      "bg-color": {
+        light: "#fff",
+        dark: "#1E313B"
+      },
+      "font-main-color": {
+        light: "#000",
+        dark: "#E2E8F0"
+      },
+      "font-additional-color": {
+        light: "#fff",
+        dark: "#E2E8F0",
+      },
+      "gray-light": {
+        light: "#F1F1F1",
+        dark: "#958b8b"
+      },
+      "gray-normal": {
+        light: "#777777",
+        dark: "#d5cfcf"
+      },
+      "gray-dark": {
+        light: "#222222",
+        dark: "#E2E8F0"
+      },
+      "green-light": "#A1C152",
+      "green-normal": "#94B447",
+      "green-dark": "#83A13B",
+      "green-darker": "#698929",
+      "orange": "#EE643A",
+      "blue": "#345EA8",
+      "red": "#DF341D",
+      "pine-green": "#2B9888"
+    },
     extend: {},
   },
   plugins: [],

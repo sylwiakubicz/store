fa89abdd1094cc933428275ea0272fc09cb8fce8
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-29 21:44:03 +0100

    Feat: Display elements in header-wrapper component.

--- store/src/app/components/header/header-wrapper/header-wrapper.component.ts
+++ store/src/app/components/header/header-wrapper/header-wrapper.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { TopInfoComponent } from '../top-info/top-info.component';
+import { HeaderComponent } from '../header/header.component';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-header-wrapper',
   standalone: true,
-  imports: [],
+  imports: [
+    TopInfoComponent,
+    HeaderComponent,
+    MenuComponent
+  ],
   templateUrl: './header-wrapper.component.html',
   styles: ``
 })

d6a09a30968dec7955cb1a03086c8d99f10b3c54
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-02 14:32:42 +0100

    Feat: Add responsiveness from 1024 to max width.

--- store/src/app/components/header/header/header.component.ts
+++ store/src/app/components/header/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderLogoComponent } from '../header-logo/header-logo.component';
 import { HeaderBasketInfoComponent } from '../header-basket-info/header-basket-info.component';
+import { HeaderSearchComponent } from '../header-search/header-search.component';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     HeaderLogoComponent,
-    HeaderBasketInfoComponent
+    HeaderBasketInfoComponent,
+    HeaderSearchComponent
   ],
   templateUrl: './header.component.html',
   styles: ``

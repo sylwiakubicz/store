7ff51a0191d865868afee19d67b8adf3a1ed5c8a
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-02 23:38:38 +0100

    Feat: Create sidebar navbar.

--- store/src/app/components/header/menu/menu.component.ts
+++ store/src/app/components/header/menu/menu.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { MenuNavbarComponent } from '../menu-navbar/menu-navbar.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [
-    MenuNavbarComponent
+    MenuNavbarComponent,
+    NgClass
   ],
   templateUrl: './menu.component.html',
   styles: ``
 })
 export class MenuComponent {
+  showNavbar : boolean = false
 
+  toggleNavber() {
+    this.showNavbar = !this.showNavbar
+  }
 }

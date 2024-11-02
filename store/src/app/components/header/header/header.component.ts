277c91db61a97c68d309685f72c0d0231a12e94b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-02 18:16:37 +0100

    Feat: Create search dropdown on small screens.

--- store/src/app/components/header/header/header.component.ts
+++ store/src/app/components/header/header/header.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderLogoComponent } from '../header-logo/header-logo.component';
 import { HeaderBasketInfoComponent } from '../header-basket-info/header-basket-info.component';
 import { HeaderSearchComponent } from '../header-search/header-search.component';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     HeaderLogoComponent,
     HeaderBasketInfoComponent,
-    HeaderSearchComponent
+    HeaderSearchComponent,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
+  searchToggle : boolean = true
 
+  handleSearchToggle()  {
+    this.searchToggle = !this.searchToggle
+  }
 }

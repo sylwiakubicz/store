b3411fe96a1d9e99f4b2dfca8bd80203da8abcb8
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-05 23:20:32 +0100

    Feat: Move dropdown arrow to separate component.

--- store/src/app/components/header/menu-navbar/menu-navbar.component.ts
+++ store/src/app/components/header/menu-navbar/menu-navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { DropdownArrowComponent } from '../../icons/dropdown-arrow/dropdown-arrow.component';
 @Component({
   selector: 'app-menu-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    DropdownArrowComponent
+  ],
   templateUrl: './menu-navbar.component.html',
   styleUrl: `./menu-navbar.component.css`
 })
---
+++ store/src/app/components/icons/dropdown-arrow/dropdown-arrow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-arrow',
+  standalone: true,
+  imports: [],
+  templateUrl: './dropdown-arrow.component.html',
+  styles: ``
+})
+export class DropdownArrowComponent {
+
+}

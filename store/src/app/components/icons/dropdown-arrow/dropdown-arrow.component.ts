fbf4ea0362c2cdb67a51c499aa1f03042fcb81c5
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-05 23:27:01 +0100

    Feat: Add prop to dropdown arrow component to rotate depends on its value.

--- store/src/app/components/icons/dropdown-arrow/dropdown-arrow.component.ts
+++ store/src/app/components/icons/dropdown-arrow/dropdown-arrow.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-dropdown-arrow',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './dropdown-arrow.component.html',
   styles: ``
 })
 export class DropdownArrowComponent {
-
+  @Input() rightArrow : boolean = false;
 }

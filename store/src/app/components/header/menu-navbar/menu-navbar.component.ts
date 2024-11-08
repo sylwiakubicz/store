303dc3a8830b9cd4a00d39f1c02a5330ab509ef6
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-08 19:50:38 +0100

    Fix: Delete all flowbite logic from menu-navbar component. Create css classes which does the same thing.

--- store/src/app/components/header/menu-navbar/menu-navbar.component.ts
+++ store/src/app/components/header/menu-navbar/menu-navbar.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit, AfterViewInit, Renderer2  } from '@angular/core';
+import { Component } from '@angular/core';
 import { DropdownArrowComponent } from '../../icons/dropdown-arrow/dropdown-arrow.component';
-import { BreakpointObserver } from '@angular/cdk/layout';
 import * as menu from "../../../../assets/data/menu.json"
 import { NgFor, NgIf, NgClass } from '@angular/common';
-
-declare var Flowbite: any;
 
 @Component({
   selector: 'app-menu-navbar',
@@ -19,36 +16,6 @@ declare var Flowbite: any;
   styleUrl: `./menu-navbar.component.css`
 })
 export class MenuNavbarComponent {
-
-  isScreenLarge: boolean = false;
   subMenuTabs: any = (menu as any).default;
-  
-  constructor (
-    private breakpointObserver: BreakpointObserver,
-    private renderer: Renderer2,
-  ) {console.log(this.subMenuTabs)}
-
-  ngOnInit(): void {
-    this.breakpointObserver.observe(['(min-width: 1024px)'])
-      .subscribe(result => {
-        this.isScreenLarge = result.matches;
-        this.updateDropdownTriggers();
-      });
-
-  }
-
-  ngAfterViewInit(): void {
-    Flowbite.initDropdowns();
-    this.updateDropdownTriggers();
-  }
-
-  updateDropdownTriggers(): void {
-    const buttons = document.querySelectorAll('.dropDownButton');
-    buttons.forEach(button => {
-      console.log("for each")
-      const triggerValue = this.isScreenLarge ? 'hover' : 'click';
-      this.renderer.setAttribute(button, 'data-dropdown-trigger', triggerValue);
-    });
-  }
 }
 

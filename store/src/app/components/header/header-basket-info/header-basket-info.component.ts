380ba2bb925fc2cffd2ab1f6edb2951b95938aa6
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-09 20:18:42 +0100

    Refactor: Move rook icon to separate component.

--- store/src/app/components/header/header-basket-info/header-basket-info.component.ts
+++ store/src/app/components/header/header-basket-info/header-basket-info.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RookIconComponent } from '../../icons/rook-icon/rook-icon.component';
 
 @Component({
   selector: 'app-header-basket-info',
   standalone: true,
-  imports: [],
+  imports: [
+    RookIconComponent
+  ],
   templateUrl: './header-basket-info.component.html',
   styles: ``
 })
---
+++ store/src/app/components/icons/rook-icon/rook-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rook-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './rook-icon.component.html',
+  styles: ``
+})
+export class RookIconComponent {
+
+}

0dcd0e87535632748c76faa0354381fd249cff5a
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-09 21:12:50 +0100

    Refactor: Move circle icon to separe component

--- store/src/app/components/header/top-info/top-info.component.ts
+++ store/src/app/components/header/top-info/top-info.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CircleIconComponent } from '../../icons/circle-icon/circle-icon.component';
 @Component({
   selector: 'app-top-info',
   standalone: true,
-  imports: [],
+  imports: [
+    CircleIconComponent
+  ],
   templateUrl: './top-info.component.html',
-  styles: ``
 })
 export class TopInfoComponent {
-
 }
---
+++ store/src/app/components/icons/circle-icon/circle-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './circle-icon.component.html',
+  styles: ``
+})
+export class CircleIconComponent {
+
+}

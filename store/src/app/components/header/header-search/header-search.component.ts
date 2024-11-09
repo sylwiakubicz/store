d2c4e27f76d01c50d9f7eb18a20fc7705686fa68
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-09 20:15:18 +0100

    Refactor: MOve search icon to separate component.

--- store/src/app/components/header/header-search/header-search.component.ts
+++ store/src/app/components/header/header-search/header-search.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SearchIconComponent } from '../../icons/search-icon/search-icon.component';
 
 @Component({
   selector: 'app-header-search',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchIconComponent
+  ],
   templateUrl: './header-search.component.html',
   styles: ``
 })
---
+++ store/src/app/components/icons/search-icon/search-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-icon.component.html',
+  styles: ``
+})
+export class SearchIconComponent {
+
+}

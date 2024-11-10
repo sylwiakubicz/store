50c0ac3c8339e9b99414c161ea57cd2f7e910653
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-10 15:00:43 +0100

    Feat: Create wand-magic-icon component.

---
+++ store/src/app/components/icons/wand-magic-icon/wand-magic-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wand-magic-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './wand-magic-icon.component.html',
+  styles: ``
+})
+export class WandMagicIconComponent {
+
+}

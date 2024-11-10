3dcbe96df6fad8f0aed5c95e5cabfd2699bd6d2b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-10 20:57:10 +0100

    Feat: Create footer-contact info component.

---
+++ store/src/app/components/footer/footer-contact-info/footer-contact-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-contact-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-contact-info.component.html',
+  styles: ``
+})
+export class FooterContactInfoComponent {
+
+}

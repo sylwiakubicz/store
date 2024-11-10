2030f6d632bc8687bc3726ccf257044a09775356
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-10 15:00:30 +0100

    Feat: Create wand-magic-sparles-icon component.

---
+++ store/src/app/components/icons/wand-magic-sparkles-icon/wand-magic-sparkles-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wand-magic-sparkles-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './wand-magic-sparkles-icon.component.html',
+  styles: ``
+})
+export class WandMagicSparklesIconComponent {
+
+}

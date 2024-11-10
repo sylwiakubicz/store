a89f6320ff57d629720c772280d398a1883d4ce4
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-10 15:01:33 +0100

    Feat: Design theme toggle button.

--- store/src/app/components/header/theme-toggle-button/theme-toggle-button.component.ts
+++ store/src/app/components/header/theme-toggle-button/theme-toggle-button.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { WandMagicIconComponent } from '../../icons/wand-magic-icon/wand-magic-icon.component';
+import { WandMagicSparklesIconComponent } from '../../icons/wand-magic-sparkles-icon/wand-magic-sparkles-icon.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-theme-toggle-button',
   standalone: true,
-  imports: [],
+  imports: [
+    WandMagicIconComponent,
+    WandMagicSparklesIconComponent,
+    NgClass
+  ],
   templateUrl: './theme-toggle-button.component.html',
   styles: ``
 })
 export class ThemeToggleButtonComponent {
+  isDarkMode : boolean = false;
 
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode
+  }
 }

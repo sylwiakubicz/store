c6cb0146437e59ddba693705dd50376a619874e1
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-10 20:31:35 +0100

    Feat: Create footer-wrapper component.

---
+++ store/src/app/components/footer/footer-wrapper/footer-wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-wrapper',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-wrapper.component.html',
+  styles: ``
+})
+export class FooterWrapperComponent {
+
+}

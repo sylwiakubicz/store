91201997b70c67fe0c4dec3893418fbb61b01bae
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-10 13:55:45 +0100

    Feat: Create theme-toggle-button component and dispaly it in top-info header component.

---
+++ store/src/app/components/header/theme-toggle-button/theme-toggle-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-toggle-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-toggle-button.component.html',
+  styles: ``
+})
+export class ThemeToggleButtonComponent {
+
+}
--- store/src/app/components/header/top-info/top-info.component.ts
+++ store/src/app/components/header/top-info/top-info.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CircleIconComponent } from '../../icons/circle-icon/circle-icon.component';
+import { ThemeToggleButtonComponent } from '../theme-toggle-button/theme-toggle-button.component';
+
 @Component({
   selector: 'app-top-info',
   standalone: true,
   imports: [
-    CircleIconComponent
+    CircleIconComponent,
+    ThemeToggleButtonComponent
   ],
   templateUrl: './top-info.component.html',
 })

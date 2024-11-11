2911c57c8bd8c40d3556eafa720bda294c8eaad1
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-11 16:47:40 +0100

    Feat: Dispaly footer on home page.

--- store/src/app/components/home-page/home-page.component.ts
+++ store/src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderWrapperComponent } from '../header/header-wrapper/header-wrapper.component';
+import { FooterWrapperComponent } from '../footer/footer-wrapper/footer-wrapper.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    HeaderWrapperComponent
+    HeaderWrapperComponent,
+    FooterWrapperComponent
   ],
   templateUrl: './home-page.component.html',
   styles: ``

a200ee44ae786b56ea9a946c8ef3753360e24973
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-11 16:19:13 +0100

    Feat: Create footer payment and delivery info componnt.

---
+++ store/src/app/components/footer/footer-payment-delivery-info/footer-payment-delivery-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-payment-delivery-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-payment-delivery-info.component.html',
+  styles: ``
+})
+export class FooterPaymentDeliveryInfoComponent {
+
+}
--- store/src/app/components/footer/footer-wrapper/footer-wrapper.component.ts
+++ store/src/app/components/footer/footer-wrapper/footer-wrapper.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterContactInfoComponent } from '../footer-contact-info/footer-contact-info.component';
 import { FooterCopyrightInfoComponent } from '../footer-copyright-info/footer-copyright-info.component';
+import { FooterPaymentDeliveryInfoComponent } from '../footer-payment-delivery-info/footer-payment-delivery-info.component';
 
 @Component({
   selector: 'app-footer-wrapper',
   standalone: true,
   imports: [
     FooterContactInfoComponent,
-    FooterCopyrightInfoComponent
+    FooterCopyrightInfoComponent,
+    FooterPaymentDeliveryInfoComponent
   ],
   templateUrl: './footer-wrapper.component.html',
   styles: ``

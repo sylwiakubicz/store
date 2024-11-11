99cfdedfea418f6550e2b74308596b6219c4e0c8
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-11 13:53:49 +0100

    Feat: Create footer copyright info component.

---
+++ store/src/app/components/footer/footer-copyright-info/footer-copyright-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-copyright-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-copyright-info.component.html',
+  styles: ``
+})
+export class FooterCopyrightInfoComponent {
+
+}
--- store/src/app/components/footer/footer-wrapper/footer-wrapper.component.ts
+++ store/src/app/components/footer/footer-wrapper/footer-wrapper.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FooterContactInfoComponent } from '../footer-contact-info/footer-contact-info.component';
+import { FooterCopyrightInfoComponent } from '../footer-copyright-info/footer-copyright-info.component';
 
 @Component({
   selector: 'app-footer-wrapper',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterContactInfoComponent,
+    FooterCopyrightInfoComponent
+  ],
   templateUrl: './footer-wrapper.component.html',
   styles: ``
 })

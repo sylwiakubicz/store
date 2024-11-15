590b5a81b39eafa990d199185ebf4dba46f975fd
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-11-15 19:46:14 +0100

    Refactor: Move star icon to separate component.

---
+++ src/app/components/icons/star-icon/star-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './star-icon.component.html',
+  styles: ``
+})
+export class StarIconComponent {
+
+}
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../interfaces/product.interface';
 import { CommonModule } from '@angular/common';
-
+import { StarIconComponent } from '../icons/star-icon/star-icon.component';
 @Component({
     selector: 'app-product',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, StarIconComponent],
     templateUrl: './product.component.html'
 })
 export class ProductComponent {
